000e76b12d7832daf0e99d9be22ba197cb8852dd
BALL EUGENE <eugene.ball@viacesi.fr>
2021-04-15 10:16:52 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace GetUserIdleTimeOnPc
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+
+namespace GetUserIdleTimeOnPc
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Stop touching the PC to see the IdleTime
+            Thread.Sleep(10000);
+            TimeSpan timespent = TimeSpan.FromMilliseconds(IdleTimeFinder.GetIdleTime());
+            Console.WriteLine($"User was idle for : {timespent.TotalSeconds} + seconds");
+
+        }
+    }
+}
